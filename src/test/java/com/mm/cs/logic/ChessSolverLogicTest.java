ad4d5c3c4c89084194831b95253182cfb59f0929
miquel.millan <miquel.millan@gmail.com>
2015-02-18 11:52:12 +0100

    Correction on the test cases

--- src/test/java/com/mm/cs/logic/ChessSolverLogicTest.java
+++ src/test/java/com/mm/cs/logic/ChessSolverLogicTest.java
@@ -12,7 +12,7 @@ import com.mm.cs.logic.impl.ChessSolverLogicImpl;
 
 public class ChessSolverLogicTest {
 
-	//@Test
+	@Test
 	public void threeSquaresTwoKingsOneRookTest(){
 		ChessSolverLogic csl = new ChessSolverLogicConcurrentImpl();
 		Set<String> configs = csl.solveChessBoard(3, 3, 0, 1, 0, 0, 2);
@@ -44,7 +44,7 @@ public class ChessSolverLogicTest {
 		assertTrue(configs.contains(config));
 	}
 	
-	//@Test
+	@Test
 	public void fourSquaresTwoRooksFourKnightsTest(){
 		ChessSolverLogic csl = new ChessSolverLogicImpl();
 		Set<String> configs = csl.solveChessBoard(4, 4, 4, 2, 0, 0, 0);
@@ -123,7 +123,7 @@ public class ChessSolverLogicTest {
 		assertEquals(configs.size(), configsConcurrent.size());
 	}
 	
-	//@Test
+	@Test
 	public void singleThreadVsConcurrentThreadComplexTest(){
 		
 		long time = System.currentTimeMillis();
